59594767af0336f963c3fdd2fad65c523c7666c8
Alexei <feoalexei@gmail.com>
2023-03-16 22:53:03 +0200

    add testing stuff

--- meadowlark-project/meadowlark.js
+++ meadowlark-project/meadowlark.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 
 const app = express();
 
@@ -18,29 +19,23 @@ app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() });
-});
+app.get('/about', handlers.about);
 
 // User page 404
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
+app.use(handlers.notFound);
 
 // User page 500
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.type('text/plain');
-  res.status(500);
-  res.render('500');
-});
+app.use(handlers.serverError);
 
-app.listen(port, () =>
-  console.log(
-    `Express started on http://localhost:${port}; ` +
-      `press Ctrl+C to terminate.`
-  )
-);
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `Express started on http://localhost:${port}` +
+        '; press Ctrl-C to terminate.'
+    );
+  });
+} else {
+  module.exports = app;
+}
