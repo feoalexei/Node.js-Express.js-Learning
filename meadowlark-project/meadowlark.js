3bad03d1fc799c944fd9143a231e21b6bca0192a
Alexei <feoalexei@gmail.com>
2023-03-23 15:02:14 +0200

    work with forms

--- meadowlark-project/meadowlark.js
+++ meadowlark-project/meadowlark.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-undef */
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
+
 const handlers = require('./lib/handlers');
 
 const app = express();
@@ -19,13 +22,11 @@ app.engine(
     },
   })
 );
-// app.engine(
-//   'handlebars',
-//   expressHandlebars.engine({
-//     defaultLayout: 'main',
-//   })
-// );
+
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
@@ -33,7 +34,25 @@ const port = process.env.PORT || 3000;
 
 app.get('/', handlers.home);
 
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
 app.get('/about', handlers.about);
+
+// vacation photo contest
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err) return res.status(500).send({ error: err.message });
+    handlers.vacationPhotoContestProcess(req, res, fields, files);
+  });
+});
 
 // User page 404
 app.use(handlers.notFound);
